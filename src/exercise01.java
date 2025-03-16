8f3eba3f3687e8e84e2701376e46ee7f958bc70b
nguyenthedat <dnguyenthe706@gmail.com>
2025-03-17 02:06:21 +0700

    1

---
+++ src/exercise01.java
@@ -0,0 +1,7 @@
+public class exercise01 {
+    public static void main(String[] args) {
+        System.out.println("welcome to computer science");
+        System.out.println("Welcome to java");
+        System.out.print("programming is fun");
+    }
+}
