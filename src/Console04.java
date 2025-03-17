59856716de3d769f383bcd42a18b665c766aabc0
nguyenthedat <dnguyenthe706@gmail.com>
2025-03-17 10:54:44 +0700

    26

---
+++ src/Console04.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Console04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("nhap so nguyen:");
+        int ngay = sc.nextInt();
+        int thang = sc.nextInt();
+        int nam = sc.nextInt();
+        System.out.printf("%d / %d / %d",ngay,thang,nam);
+    }
+}
