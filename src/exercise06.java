c77383f88d6775aa90afef9cf6f17d8402592c44
nguyenthedat <dnguyenthe706@gmail.com>
2025-03-17 09:55:40 +0700

    6

---
+++ src/exercise06.java
@@ -0,0 +1,15 @@
+public class exercise06 {
+    public static void main(String[] args) {
+        System.out.println("" + ( 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9));
+
+
+
+        System.out.println("" + 1000000L * (1000000 + 1) / 2);
+
+
+        System.out.println("" + (9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5));
+
+
+        System.out.println("" + 4 * (1 - 1/3.0 + 1.0/5  - 1.0/7 + 1.0/9 - 1.0/11 + 1.0/13 ));
+    }
+}
