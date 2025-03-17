648aaae58bd437e043330719d1ee35972d28b9a4
nguyenthedat <dnguyenthe706@gmail.com>
2025-03-17 11:02:16 +0700

    211

---
+++ src/sdf.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Console01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("nhap gia tri:");
+        float myint = sc.nextFloat();
+        System.out.printf("%.2f",myint * 2.54);
+        System.out.print("cm");
+
+
+    }
+}
+
