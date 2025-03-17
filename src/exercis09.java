cd053eab1494b21c5c1575d4ec4cc02f68eb5180
nguyenthedat <dnguyenthe706@gmail.com>
2025-03-17 09:59:08 +0700

    9

---
+++ src/exercis09.java
@@ -0,0 +1,14 @@
+public class exercis09 {
+    public static void main(String[] args) {
+        System.out.print("nam 1 :" + (312032486 + 31536000 / 6 - 31536000 / 15 + 31536000 / 40));
+        System.out.println(" nguoi");
+        System.out.print("nam 2 :" + (312032486 + 31536000 / 6 - 31536000 / 15 + 31536000 / 40) + (31536000 / 6 - 31536000 / 15 + 31536000 / 40));
+        System.out.println(" nguoi");
+        System.out.print("nam 3 :" + (312032486 + 31536000 / 6 - 31536000 / 15 + 31536000 / 40) + (31536000 / 6 - 31536000 / 15 + 31536000 / 40) * 2);
+        System.out.println(" nguoi");
+        System.out.print("nam 4 :" + (312032486 + 31536000 / 6 - 31536000 / 15 + 31536000 / 40) + (31536000 / 6 - 31536000 / 15 + 31536000 / 40) * 3);
+        System.out.println(" nguoi");
+        System.out.print("nam 5 :" + (312032486 + 31536000 / 6 - 31536000 / 15 + 31536000 / 40) + (31536000 / 6 - 31536000 / 15 + 31536000 / 40) * 4);
+        System.out.println(" nguoi");
+    }
+}
