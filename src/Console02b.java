0c613f26252897894865cb9479fd631abaec7f35
nguyenthedat <dnguyenthe706@gmail.com>
2025-03-17 10:51:30 +0700

    24

---
+++ src/Console02b.java
@@ -0,0 +1,16 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class Console02b {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("nhap do C:");
+        float doc = sc.nextFloat();
+        System.out.printf("%.2f",(9 * doc) / 5 + 32);
+
+
+    }
+
+}
