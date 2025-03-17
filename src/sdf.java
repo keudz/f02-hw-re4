7cff2d177c69f903bb4c78c4eb604ba8515d54e4
nguyenthedat <dnguyenthe706@gmail.com>
2025-03-17 10:58:06 +0700

    28

---
+++ src/sdf.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Console05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String ten = sc. nextLine();
+        int tuoi = sc.nextInt();
+        float chieucao = sc.nextFloat();
+
+        System.out.printf(" ho va ten : %s\n", ten);
+
+        System.out.printf("tuoi:%d\n",tuoi);
+
+        System.out.printf(" chieu cao : %.2f\n",chieucao);
+
+
+    }
+}
