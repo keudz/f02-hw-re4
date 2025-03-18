30b42bbd3cb07d38cb7900dba1df87e897359b3b
nguyenthedat <dnguyenthe706@gmail.com>
2025-03-18 20:46:34 +0700

    411

---
+++ src/Ex11.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ex11 {
+    public static void main(String[] args) {
+        System.out.println("nhap 1 so bat ky:");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for( int i = 2  ; i < n; i++ ) {
+            for( int j = 1; j <= Math.sqrt( i); j++){
+                if( j * j == i){
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+}// xong
+
