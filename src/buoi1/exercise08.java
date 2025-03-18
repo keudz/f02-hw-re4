c70bd9461baf95f45d28e08f3a754647db0c8c7e
nguyenthedat <dnguyenthe706@gmail.com>
2025-03-18 23:11:14 +0700

    811

--- src/buoi1/exercise08.java
+++
@@ -1,6 +0,0 @@
-public class exercise08 {
-    public static void main(String[] args) {
-        System.out.print("" + ( 1.6 * 24) / ( 1 + 40.0 / 60 + 35.0 / 3600));
-        System.out.print(" km /  gio");
-    }
-}
