29d3fb086ab1827a4e794d663f549f2ab6fc6930
nguyenthedat <dnguyenthe706@gmail.com>
2025-03-18 20:38:42 +0700

    45

---
+++ src/Ex05.java
@@ -0,0 +1,9 @@
+public class Ex05 {
+    public static void main(String[] args) {
+        int n = 1;
+        while( n * n * n < 12000 ){
+            n++;
+        }
+        System.out.println("so lon nhat la: "+ (n - 1) );
+    }
+}//xong
