a548c66de65dc588f7ce6681ed6edbaf8390743f
nguyenthedat <dnguyenthe706@gmail.com>
2025-03-18 23:08:45 +0700

    89

--- src/buoi1/exercise04.java
+++
@@ -1,7 +0,0 @@
-public class exercise04 {
-    public static void main(String[] args) {
-        System.out.println("chu vi hinh tron:" + ( 2 * Math.PI * 5.5 ));
-        System.out.println("dien tich hinh tron:" + ( Math.PI * 5.5 * 5.5 ));
-
-    }
-}
