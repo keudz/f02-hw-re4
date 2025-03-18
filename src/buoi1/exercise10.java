541418ddeafd9f1f852610ef5dd7c1a7989714ab
nguyenthedat <dnguyenthe706@gmail.com>
2025-03-18 23:13:31 +0700

    813

--- src/buoi1/exercise10.java
+++
@@ -1,6 +0,0 @@
-public class exercise10 {
-    public static void main(String[] args) {
-        System.out.println(" x = " + ((44.5 * 0.55) - (50.2 *5.9)) / ((3.4 * 0.55) - (50.2*2.1)));
-        System.out.print(" y = " + ((3.4 * 5.9) - ( 44.5 * 2.1)) / ((3.4 * 0.55) - (50.2*2.1)));
-    }
-}
